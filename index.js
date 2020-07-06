038914b73db96dc78b217c836cc6d8dfc4ba4e16
CourtneyWatts <courtney.watts@icloudm>
2020-07-06 18:59:37 +0100

    axios call  and debounce function set up

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('test')
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'df76929f',
+      s: searchTerm,
+    },
+  })
+  return response.data.Search
+}
+
+const input = document.querySelector('#input1')
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value)
+  for (let movie of movies) {
+    const div = document.createElement('div')
+    div.innerHTML = `
+     <img src="${movie.Poster}" />
+     <h1>${movie.Title}</h1>
+    `
+    document.querySelector('#target').appendChild(div)
+  }
+}
+
+input.addEventListener('input', debounce(onInput, 500))
