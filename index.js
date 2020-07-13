f6d9f1bdc3968f97dba27317a9386364034b75a9
CourtneyWatts <courtney.watts@icloudm>
2020-07-13 16:40:39 +0100

    dropdown menu, clickaway close + follow up request

--- index.js
+++ index.js
@@ -5,21 +5,67 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   })
+  if (response.data.Error) {
+    return []
+  }
   return response.data.Search
 }
 
-const input = document.querySelector('#input1')
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+  <label><b>Search For A Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+      </div>
+  </div>
+`
+
+const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value)
+  if (!movies.length) {
+    dropdown.classList.remove('is-active')
+    return
+  }
+
+  resultsWrapper.innerHTML = ''
+  dropdown.classList.add('is-active')
   for (let movie of movies) {
-    const div = document.createElement('div')
-    div.innerHTML = `
-     <img src="${movie.Poster}" />
-     <h1>${movie.Title}</h1>
+    const option = document.createElement('a')
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+    option.classList.add('dropdown-item')
+    option.innerHTML = `
+     <img src="${imgSrc}" />
+     ${movie.Title}
     `
-    document.querySelector('#target').appendChild(div)
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active')
+      input.value = movie.Title
+      onMovieSelect(movie)
+    })
+    resultsWrapper.appendChild(option)
   }
 }
 
 input.addEventListener('input', debounce(onInput, 500))
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active')
+  }
+})
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'df76929f',
+      i: movie.imdbID,
+    },
+  })
+  console.log(response)
+}
