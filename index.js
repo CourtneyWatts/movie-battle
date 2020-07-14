616d758c0927eb52c34f22bd332697b5e8e26a76
CourtneyWatts <courtney.watts@icloudm>
2020-07-14 10:00:33 +0100

    rendering summary and statistics

--- index.js
+++ index.js
@@ -68,4 +68,44 @@ const onMovieSelect = async (movie) => {
     },
   })
   console.log(response)
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+      <img src="${movieDetail.Poster}" />
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+    `
 }
