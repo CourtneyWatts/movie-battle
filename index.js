a017fc57127693ec23997d0d69ebd6b29fda5ccd
CourtneyWatts <courtney.watts@icloudm>
2020-07-14 13:50:58 +0100

    extracting rendering logic

--- index.js
+++ index.js
@@ -11,53 +11,15 @@ const fetchData = async (searchTerm) => {
   return response.data.Search
 }
 
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-  <label><b>Search For A Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-      </div>
-  </div>
-`
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const resultsWrapper = document.querySelector('.results')
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value)
-  if (!movies.length) {
-    dropdown.classList.remove('is-active')
-    return
-  }
-
-  resultsWrapper.innerHTML = ''
-  dropdown.classList.add('is-active')
-  for (let movie of movies) {
-    const option = document.createElement('a')
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-    option.classList.add('dropdown-item')
-    option.innerHTML = `
-     <img src="${imgSrc}" />
-     ${movie.Title}
-    `
-    option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active')
-      input.value = movie.Title
-      onMovieSelect(movie)
-    })
-    resultsWrapper.appendChild(option)
-  }
-}
-
-input.addEventListener('input', debounce(onInput, 500))
-
-document.addEventListener('click', (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active')
-  }
+    return `
+    <img src="${imgSrc}" />
+    ${movie.Title} (${movie.Year})
+   `
+  },
 })
 
 const onMovieSelect = async (movie) => {
