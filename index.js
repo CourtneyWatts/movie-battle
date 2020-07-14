2802ca2b7c579222b051ff20ee88124bebbca798
CourtneyWatts <courtney.watts@icloudm>
2020-07-14 15:43:48 +0100

    extracted fetch function

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'df76929f',
-      s: searchTerm,
-    },
-  })
-  if (response.data.Error) {
-    return []
-  }
-  return response.data.Search
-}
-
-createAutoComplete({
+const fetchData = createAutoComplete({
   root: document.querySelector('.autocomplete'),
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
@@ -25,6 +12,18 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title
+  },
+  async fetchData(searchTerm){
+      const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+          apikey: 'df76929f',
+          s: searchTerm,
+        },
+      })
+      if (response.data.Error) {
+        return []
+      }
+      return response.data.Search
   },
 })
 
