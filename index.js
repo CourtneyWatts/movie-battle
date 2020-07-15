4c2c32648e39cf5e5609fecea47b0737d7a67e4a
CourtneyWatts <courtney.watts@icloudm>
2020-07-15 12:01:42 +0100

    simplifying autoComplete config

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const fetchData = createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
     return `
@@ -13,19 +12,29 @@ const fetchData = createAutoComplete({
   inputValue(movie) {
     return movie.Title
   },
-  async fetchData(searchTerm){
-      const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-          apikey: 'df76929f',
-          s: searchTerm,
-        },
-      })
-      if (response.data.Error) {
-        return []
-      }
-      return response.data.Search
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'df76929f',
+        s: searchTerm,
+      },
+    })
+    if (response.data.Error) {
+      return []
+    }
+    return response.data.Search
   },
+}
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
 })
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+})
+
 
 const onMovieSelect = async (movie) => {
   const response = await axios.get('http://www.omdbapi.com/', {
