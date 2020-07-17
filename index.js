21532515400d944435bfa29a06cfa0850e6e2ebc
CourtneyWatts <courtney.watts@icloudm>
2020-07-17 11:34:41 +0100

    setting response fields to comparable integer vals

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ createAutoComplete({
   root: document.querySelector('#left-autocomplete'),
   onOptionSelect(movie) {
     document.querySelector('.tutorial').classList.add('is-hidden')
-    onMovieSelect(movie, document.querySelector('#left-summary'))
+    onMovieSelect(movie, document.querySelector('#left-summary'), 'left')
   },
 })
 createAutoComplete({
@@ -37,11 +37,13 @@ createAutoComplete({
   root: document.querySelector('#right-autocomplete'),
   onOptionSelect(movie) {
     document.querySelector('.tutorial').classList.add('is-hidden')
-    onMovieSelect(movie, document.querySelector('#right-summary'))
+    onMovieSelect(movie, document.querySelector('#right-summary'), 'right')
   },
 })
+let leftMovie
+let rightMovie
 
-const onMovieSelect = async (movie, summaryElement) => {
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'df76929f',
@@ -50,9 +52,36 @@ const onMovieSelect = async (movie, summaryElement) => {
   })
   console.log(response)
   summaryElement.innerHTML = movieTemplate(response.data)
+  if (side === 'left') {
+    leftMovie = response.data
+  } else {
+    rightMovie = response.data
+  }
+  if (leftMovie && rightMovie) {
+    runComparison()
+  }
+}
+
+const runComparison = () => {
+  console.log('now-compare')
 }
 
 const movieTemplate = (movieDetail) => {
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
+  )
+  const metascore = parseInt(movieDetail.Metascore)
+  const imdbRating = parseFloat(movieDetail.imdbRating)
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
+  const awards = movieDetail.Awards.split(' ').reduce((prev,word) => {
+    const value = parseInt(word)
+    if(isNaN(value)){
+      return prev
+    } else {
+      return prev + value
+    }
+  }, 0)
+  console.log(awards)
   return `
   <article class="media">
     <figure class="media-left">
