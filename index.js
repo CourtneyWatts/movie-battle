27005654448bd467d1fa18b9e9710c95d10641fc
CourtneyWatts <courtney.watts@icloudm>
2020-07-18 14:25:40 +0100

    style changes, added preview images

--- index.js
+++ index.js
@@ -63,25 +63,55 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 }
 
 const runComparison = () => {
-  console.log('now-compare')
+  const leftSideStats = document.querySelectorAll('#left-summary .notification')
+  const rightSideStats = document.querySelectorAll(
+    '#right-summary .notification'
+  )
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index]
+    const leftSideValue = parseFloat(leftStat.dataset.value)
+    const rightSideValue = parseFloat(rightStat.dataset.value)
+    console.log(`left:${leftSideValue}, right:${rightSideValue}`)
+    if (isNaN(rightSideValue) || isNaN(leftSideValue)) {
+      leftStat.classList.remove('is-primary')
+      leftStat.classList.add('is-light')
+      rightStat.classList.remove('is-primary')
+      rightStat.classList.add('is-light')
+    } else if (rightSideValue > leftSideValue) {
+      rightStat.classList.remove('is-danger')
+      rightStat.classList.add('is-primary')
+      leftStat.classList.remove('is-primary')
+      leftStat.classList.add('is-danger')
+    } else if (leftSideValue > rightSideValue) {
+      leftStat.classList.remove('is-danger')
+      leftStat.classList.add('is-primary')
+      rightStat.classList.remove('is-primary')
+      rightStat.classList.add('is-danger')
+    } else if (leftSideValue === rightSideValue) {
+      leftStat.classList.remove('is-primary', 'is-danger')
+      leftStat.classList.add('is-light')
+      rightStat.classList.remove('is-primary', 'is-danger')
+      rightStat.classList.add('is-light')
+    }
+  })
 }
 
 const movieTemplate = (movieDetail) => {
   const dollars = parseInt(
     movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
   )
-  const metascore = parseInt(movieDetail.Metascore)
-  const imdbRating = parseFloat(movieDetail.imdbRating)
-  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
-  const awards = movieDetail.Awards.split(' ').reduce((prev,word) => {
+  const metascore = parseInt(movieDetail.Metascore) || 0
+  const imdbRating = parseFloat(movieDetail.imdbRating) || 0
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, '')) || 0
+  const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
     const value = parseInt(word)
-    if(isNaN(value)){
+    if (isNaN(value)) {
       return prev
     } else {
       return prev + value
     }
   }, 0)
-  console.log(awards)
   return `
   <article class="media">
     <figure class="media-left">
@@ -97,23 +127,23 @@ const movieTemplate = (movieDetail) => {
       </div>
     </div>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${awards} class="awards notification is-light">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${dollars} class="notification is-light">
     <p class="title">${movieDetail.BoxOffice}</p>
-    <p class="subtitle">Box Office</p>
+    <p class="subtitle">US Box Office</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${metascore} class="notification is-light">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${imdbRating} class="notification is-light">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${imdbVotes} class="notification is-light">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
   </article>
